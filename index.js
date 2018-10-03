a7c2c9dbe756e43c9f8a3b6a51d5b4c85239d74d
Timo <timo@teatwig.net>
2018-10-03 23:06:03 +0200

    Add package.json and eslint, formatted all JS files

---
+++ index.js
@@ -0,0 +1,8 @@
+const Bot = require('./bot')
+const TagChecker = require('./tagChecker')
+
+const config = require('./config.json')
+const sitesByChatId = require('./data.json')
+
+const tagChecker = new TagChecker(sitesByChatId)
+const bot = new Bot(config.bot, tagChecker)
